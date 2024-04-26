16d6e8bff834292934eca79b6dddec366e0deafd
Rasul0101 <rasul.aghamammadli@gmail.com>
2024-04-26 16:35:25 +0400

    Gift Card Inner page added

--- src/pages/GiftCards/GiftCardIn.jsx
+++ src/pages/GiftCards/GiftCardIn.jsx
@@ -1,7 +1,15 @@
 // import React from 'react'
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+
+// Icons
+import { IoChevronDownCircleOutline } from "react-icons/io5";
+import { RiShareBoxLine } from "react-icons/ri";
+import { BsStars } from "react-icons/bs";
+
+// components
+import ProgressLinear from "../../components/Loading/ProgressLinear";
 
 const GiftCardIn = () => {
   const { id, category } = useParams();
@@ -21,12 +29,113 @@ const GiftCardIn = () => {
       });
   }, [id]);
 
-  //   console.log(giftCategory);
+  console.log(giftCategory);
 
   return (
-    <div>
-      <img src={gift?.img} alt="" />
-    </div>
+    <>
+      <ProgressLinear />
+      <section className="gift-card-in">
+        <div className="content">
+          <nav className="breadcrumbs">
+            <Link to="/gift">Gift</Link>
+            <span>/</span>
+            <p>Create eGift</p>
+          </nav>
+          <h1 className="title">Create eGift</h1>
+          <div className="img">
+            <img src={gift.img} alt="" />
+          </div>
+          <form action="">
+            <p className="text">* indicates required field</p>
+            <div className="form-main">
+              <h2 className="head">
+                <span>Gift amount</span>
+              </h2>
+              <div className="form-group">
+                <div className="group-in">
+                  <div className="field">
+                    <select name="" id="">
+                      <option value="">$25</option>
+                      <option value="">$10</option>
+                      <option value="">$25</option>
+                      <option value="">$50</option>
+                      <option value="">$100</option>
+                      <option value="">Custom Amount</option>
+                    </select>
+                    <label>*Select gift amount</label>
+                    <IoChevronDownCircleOutline className="select-icon" />
+                  </div>
+                </div>
+              </div>
+              <h2 className="head">
+                <span>Who are you gifting to?</span>
+              </h2>
+              <div className="form-group">
+                <div className="group-in">
+                  <div className="field">
+                    <input type="text" placeholder="Recipient Name" />
+                    <label>*Recipient Name</label>
+                  </div>
+                </div>
+                <div className="group-in">
+                  <div className="field">
+                    <input type="text" placeholder="Recipient Email" />
+                    <label>*Recipient Email</label>
+                  </div>
+                </div>
+              </div>
+              <div className="add-group">
+                <p className="text">Maximum of 10 recipients</p>
+                <Link className="btn">Add another recipient</Link>
+              </div>
+              <h2 className="head">
+                <span>Personal note</span>
+              </h2>
+              <div className="form-group">
+                <div className="group-in">
+                  <div className="field">
+                    <textarea
+                      cols="30"
+                      rows="10"
+                      placeholder="Message (optional)"
+                    ></textarea>
+                  </div>
+                </div>
+              </div>
+              <h2 className="head">
+                <span>From</span>
+              </h2>
+              <div className="form-group">
+                <div className="group-in">
+                  <div className="field">
+                    <input type="text" placeholder="*Sender Name" />
+                    <label>*Sender Name</label>
+                  </div>
+                </div>
+                <div className="group-in">
+                  <div className="field">
+                    <input type="text" placeholder="*Sender Email" />
+                    <label>*Sender Email</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </section>
+      <section className="terms">
+        <div className="content">
+          <p className="text">
+            <BsStars className="stars-icon" />
+            By purchasing this eGift, I have read and agree to the Starbucks
+            Card Terms & Conditions.
+          </p>
+          <Link className="btn">
+            Card Terms & Conditions <RiShareBoxLine className="icon" />
+          </Link>
+        </div>
+      </section>
+    </>
   );
 };
 
